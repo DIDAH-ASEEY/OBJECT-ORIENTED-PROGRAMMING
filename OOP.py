36289968ad0d346f46e3293718f8545979a2134b
DIDAH-ASEEY <aseeyd@gmail.com>
2017-01-17 22:26:41 +0300

    A simple OOP program for voter registration

---
+++ OOP.py
@@ -0,0 +1,53 @@
+#voter regitration app
+
+import random
+
+class Voter(object):
+    year = 2017
+    number = 0
+    
+    
+    
+    def __init__(self,name="Voter",id_number=0):
+        self.name = name
+        self.id_number = id_number
+
+    def capture_details(self,age,residence):
+        self.age = age
+        self.residence = residence
+
+        print "Age and Residence details captured"
+
+    def generate_voter(self,number = 0):
+        
+        self.number=number
+        self.number = random.randint(1000,1000000000)
+        return self.number
+
+    def print_details(self):
+        print "Voter Name: " + self.name
+        print "Voter id_number: " + str(self.id_number)
+        print "voter Age: " + str(self.age)
+        print "VOter Residence: " + self.residence
+        print "voter number: " + str(self.number)
+
+        
+
+
+class Aspirant(Voter):
+    def ward(self,ward,party):
+        self.ward = ward
+        self.party = party
+
+    def position(self,position):
+        self.position=position
+
+    def describe_aspirant(self):
+        print "Aspirant %s is %s years old, Id Number %s from %s is vying for % seat for %s ward" %(self.name,self.age,str(self.id_number),self.residence,self.position,self.ward) 
+    
+        print "Aspirant's voter number is: %s" %str(self.number)
+        
+
+    
+
+    
